c0abd1209000c624249b7fd6029aae3e51bfb403
MEDM <m.e.misiak@gmail.com>
2019-07-25 12:37:22 +0200

    Dodanie nowej funkcjonalności: wyszukanie i edycja danych wybranego użytkownika

--- src/main/java/pl/javastart/equipy/controller/UserController.java
+++ src/main/java/pl/javastart/equipy/controller/UserController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,8 +40,18 @@ public class UserController {
         return ResponseEntity.created(location).body(dto);
     }
     
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findUser(@PathVariable Long id) {
+    	return ResponseEntity.ok(userService.findUser(id));
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+    	return ResponseEntity.ok(userService.updateUser(id, userDTO));
+    }
+    
     @GetMapping
-    public ResponseEntity<List<UserDTO>> findAllUser(@RequestParam(required = false) String lastName) {
+    public ResponseEntity<List<UserDTO>> findAllUsers(@RequestParam(required = false) String lastName) {
     	if(lastName != null) {
     		return ResponseEntity.ok(userService.findAllUsersByLastName(lastName));
     	} else {
--- src/main/java/pl/javastart/equipy/service/UserService.java
+++ src/main/java/pl/javastart/equipy/service/UserService.java
@@ -1,6 +1,7 @@
 package pl.javastart.equipy.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,7 @@ import org.springframework.stereotype.Service;
 import pl.javastart.equipy.model.dto.UserDTO;
 import pl.javastart.equipy.model.entity.User;
 import pl.javastart.equipy.repository.UserRepository;
-import pl.javastart.equipy.utils.exception.BadRequestException;
-import pl.javastart.equipy.utils.exception.ConflictException;
+import pl.javastart.equipy.utils.exception.*;
 import pl.javastart.equipy.utils.mapper.UserMapper;
 
 @Service
@@ -35,6 +35,31 @@ public class UserService {
 		User userEntity = userMapper.toEntity(userDTO);
 		User savedUser = userRepository.save(userEntity);
 		return userMapper.toDTO(savedUser);
+	}
+	
+	public UserDTO findUser(Long id) {
+		Optional<User> searchResult = userRepository.findById(id);
+		if(!searchResult.isPresent()) {
+			throw new NotFoundException("Użytkownik o wskazanym id nie istnieje");
+		} else {
+			return searchResult.map(userMapper::toDTO).get();
+		}
+	}
+	
+	public UserDTO updateUser(Long id, UserDTO userDTO) {
+		if(id != userDTO.getId()) {
+			throw new BadRequestException("Aktualizowany obiekt musi mieć id zgodne z id w ścieżce zasobu");
+		}
+		if(userRepository.existsByPesel(userDTO.getPesel())) {
+			throw new ConflictException("Inny użytkownik z takim peselem już istnieje");
+		}
+		Optional<User> searchResult = userRepository.findById(id);
+		User userEntity = searchResult.get();
+		userEntity.setFirstName(userDTO.getFirstName());
+		userEntity.setLastName(userDTO.getLastName());
+		userEntity.setPesel(userDTO.getPesel());
+	    User updatedUser = userRepository.save(userEntity);
+	    return userMapper.toDTO(updatedUser);
 	}
 	
 	public List<UserDTO> findAllUsers() {
